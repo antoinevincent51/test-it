62f9f44d1d5e0c5d070aa1d402e27272ea8856ae
antoinevincent51 <antoine.vincent51@gmail.com>
2026-02-16 11:56:15 +0100

    Update SecurityConfig.java

--- src/main/config/SecurityConfig.java
+++ src/main/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 @Configuration
 public class SecurityConfig {
         @Bean
-public SecurityFilterChain filterChain(HttpSecurity http)
-        throws Exception{
-        http
-        .authorizeHttpRequests((authz)->authz.anyRequest().authenticated()) //On demande que toute les sessions soit authentifiée
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//On rend les session stateless
-        
-        return http.build();
+        public SecurityFilterChain filterChain(HttpSecurity http)
+                throws Exception{
+                http
+                .authorizeHttpRequests((authz)->authz.anyRequest().authenticated()) //On demande que toute les sessions soit authentifiée
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//On rend les session stateless
+                return http.build();
         }
 
 }
+
 
