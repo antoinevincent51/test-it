c2645901a4dbdfe064de47e624ff21d006113bb0
antoinevincent51 <antoine.vincent51@gmail.com>
2026-02-16 10:05:41 +0100

    Update UserDetailService.java

--- src/main/java/com/example/testit/service/UserDetailService.java
+++ src/main/java/com/example/testit/service/UserDetailService.java
@@ -1,1 +1,25 @@
+package com.example.testit.service;
 
+import com.example.testit.model.User;
+import com.example.testit.repository.UserRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailService {
+   @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return user;
+    }
+}
