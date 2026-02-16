1670f2bcb9199b67cabdff30c5c209fd73787469
antoinevincent51 <antoine.vincent51@gmail.com>
2026-02-16 09:02:15 +0100

    Add files via upload

---
+++ src/main/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+public class SecurityConfig {
+        @Bean
+public SecurityFilterChain filterChain(HttpSecurity http)
+        throws Exception{
+        http
+        .authorizeHttpRequests((authz)->authz.anyRequest().authenticated()) //On demande que toute les sessions soit authentifiée
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//On rend les session stateless
+        
+        return http.build();
+        }
+
+}
